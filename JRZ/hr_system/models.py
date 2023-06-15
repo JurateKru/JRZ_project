0f23af7044bbdb56af101cf621e1671948bec944
Kobrius11 <kobrius11@gmail.com>
2023-06-15 14:53:57 +0300

    initial template

--- JRZ/hr_system/models.py
+++ JRZ/hr_system/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.urls import reverse
+from tinymce.models import HTMLField
 
 
 class Manager(models.Model):
@@ -63,30 +64,17 @@ class Employee(models.Model):
     def get_absolute_url(self):
         return reverse("employee_detail", kwargs={"pk": self.pk})
 
-class AplicationForms(models.Model):
-
-    
-
-    class Meta:
-        verbose_name = _("aplicationforms")
-        verbose_name_plural = _("aplicationforms")
-
-    def __str__(self):
-        return self.name
-
-    def get_absolute_url(self):
-        return reverse("aplicationforms_detail", kwargs={"pk": self.pk})
-
-
 
 class Application(models.Model):
+    CHOICES_TEMPLATES =(
+        (0, _("Vacation form")),
+        (1, _("Termination form"))
+                        )
     date_created = models.DateField(_("date_created"), auto_now=False, auto_now_add=False)
-    # department = models.ForeignKey( 
-    #     verbose_name=_("employee"), 
-    #     on_delete=models.CASCADE,
-    #     related_name='employees',
-    #     choices=
-    # )
+    template = models.CharField(_("template"), max_length=50, choices=CHOICES_TEMPLATES, db_index=True)
+    content = models.TextField(_("content"), null=True, blank=True)
+
+    
 
     class Meta:
         verbose_name = _("application")
@@ -97,6 +85,11 @@ class Application(models.Model):
 
     def get_absolute_url(self):
         return reverse("application_detail", kwargs={"pk": self.pk})
+
+    def get_application_form(self):
+        if self.template == 0:
+            self.content = "labas as atostogu forma"
+    
 
 
 
