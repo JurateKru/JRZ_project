4b269167ee9fba9d8b4124582513a993bd42902f
Raimonda <raimondaastramskaite@gmail.com>
2023-06-15 16:36:30 +0300

    about_us.html content created

--- JRZ/hr_system/views.py
+++ JRZ/hr_system/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'hr_system/index.html')
+
+def about_us(request):
+    return render(request, 'hr_system/about_us.html')
