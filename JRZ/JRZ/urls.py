c4f417d0be8779dc4068254eca88a19a20a66cc5
Raimonda <raimondaastramskaite@gmail.com>
2023-06-16 13:54:26 +0300

    created user_profile app

--- JRZ/JRZ/urls.py
+++ JRZ/JRZ/urls.py
@@ -22,6 +22,8 @@ from . import settings
 urlpatterns = [
     path('', include('hr_system.urls')),
     path('admin/', admin.site.urls),
+    path('profile/', include('user_profile.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('tinymce/', include('tinymce.urls')), 
 ] + (static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)\
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
