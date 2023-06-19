72659c8722e4a4f2a76aec2088c4b037f2d48245
Kobrius11 <kobrius11@gmail.com>
2023-06-19 21:26:15 +0300

    ApplicationFormView fully functional

--- JRZ/hr_system/forms.py
+++ JRZ/hr_system/forms.py
@@ -4,18 +4,19 @@ from django.utils.translation import gettext_lazy as _
 
 
 class ApplicationInstanceForm(forms.ModelForm):
-    # Vacation
-    start_date = forms.DateField(label=_("Start Date"))
-    end_date = forms.DateField(label=_("End Date"))
-    payout_before = forms.BooleanField(label=_("Payout Before"))
+    #Vacation
+    # start_date = forms.DateField(label=_("Start Date"))
+    # end_date = forms.DateField(label=_("End Date"))
+    # payout_before = forms.BooleanField(label=_("Payout Before"))
     #Taxes
-    npd = forms.BooleanField(label=_("npd"))
-    start_date = forms.DateField(label=_("start_date"))
+    # npd = forms.BooleanField(label=_("npd"))
+    # start_date = forms.DateField(label=_("start_date"))
 
 
     class Meta:
         model = models.ApplicationInstance
-        fields = ['content', 'start_date', 'end_date', 'payout_before', 'application']
+        fields = ['content', 'date_created'] # ['content', 'start_date', 'end_date', 'payout_before', 'application']
         widgets = {
             'content': forms.HiddenInput(),
+            'date_created': forms.HiddenInput(),
         }
--- JRZ/hr_system/views.py
+++ JRZ/hr_system/views.py
@@ -1,4 +1,5 @@
-from typing import Optional, Type
+from typing import Any, Optional, Type
+from django.utils.translation import gettext_lazy as _
 from django.forms.models import BaseModelForm
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
@@ -6,6 +7,7 @@ from . models import Application, ApplicationInstance
 from django.db.models import Q
 from . forms import ApplicationInstanceForm
 from django.urls import reverse, reverse_lazy
+from django import forms
 
 def index(request):
     return render(request, 'hr_system/index.html')
@@ -47,56 +49,66 @@ class ApplicationFormView(generic.CreateView):
     template_name = 'hr_system/application_instance.html'
     success_url = reverse_lazy('application_list')
 
-    # def get_form(self, form_class= form_class) -> BaseModelForm:
-    #     form = super().get_form(form_class)
-    #     application = get_object_or_404(Application, pk=self.kwargs['pk'])
-    #     if form.application == 1:
-    #         # form = form(initial={'client': forms.HiddenInput() })
-    #         form.fields['client'].widget = forms.HiddenInput() 
-    #     return form
+    # Get Aplications object by Application.id(passed throught urls.py)
+    def get_application(self):
+        return get_object_or_404(Application, pk=self.kwargs['pk'])
 
+    # Create initial form instance populate with needed fields 
+    def get_form(self, form_class= form_class) -> BaseModelForm:
+        form = super().get_form(form_class)
+        
+        if self.get_application().id == 1: # Atostogos
+            # form = form(initial={'client': forms.HiddenInput() })
+            # form= ApplicationInstanceForm(initial={'start_date': forms.DateField(label=_("Start Date")),
+            #                 'end_date': forms.DateField(label=_("End Date")),
+            #                 'payout_before': forms.DateField(label=_("Payout Before"))})
+            
+            form.fields['start_date'] = forms.DateField(label=_("Start Date"))
+            form.fields['end_date'] = forms.DateField(label=_("End Date"))
+            form.fields['payout_before'] = forms.BooleanField(label=_("Payout Before"))
+            form.fields['full_name'] = forms.CharField(label=_("full name"))
+            form.fields['manager'] = forms.CharField(label=_("manager"))
+        
+        if self.get_application().id == 2: # Mokesciai
+            form.fields['npd'] = forms.BooleanField(label=_("npd"))
+            form.fields['start_date'] = forms.DateField(label=_("start_date"))
+            # form.fields['Date_created'] = form.fields['date_created']
+        return form
+
+    # gets populated form, passes throught generate_description() returns form with values
     def form_valid(self, form):
         # application_id = self.request.POST.get('application')
-        application = get_object_or_404(Application, pk=self.kwargs['pk'])
+        # application = get_object_or_404(Application, pk=self.kwargs['pk'])
         instance = form.save(commit=False)
-        instance.application = application
+        instance.application = self.get_application()
         instance.save()
         # self.content = self.generate_description(form.cleaned_data)
         form.instance.content = self.generate_description(form.cleaned_data)
         return super().form_valid(form)
 
+    # Populates form with values(user inputs)
     def generate_description(self, form_data):
-        templates = {'vacation':"Pareigos\nPAKEISK_MANE\n\nUAB ĮMONĖ\ndirektoriui PAKEISK_MANE\n\nPRAŠYMAS DĖL ATOSTOGŲ\n\n{start_date}\n{end_date}\n\nPrašau leisti mane kasmetinių atostogų nuo {start_date} iki {end_date} imtinai. Prašau man priklausančius atostoginius išmokėti kartu su darbo užmokesčio mokėjimu.\n\nPAKEISKMANE\n",
-                     'taxes': 'taxes lalsalsldf'}
-        application = get_object_or_404(Application, pk=self.kwargs['pk'])
-        if application.title == 'Vacation':
-            template = templates['vacation']
-
-            # full_name = f"{self.request.user.first_name} {self.request.user.last_name}"
-            start_date = form_data['start_date']
-            end_date = form_data['end_date']
-
-            description = template.format(
-                start_date=start_date,
-                end_date=end_date
-            ) # full_name=full_name,
-            return description
-
-        if application.title == 'Taxes form':
-            template = templates['taxes']
-
-            npd = form_data['npd']
-            start_date = form_data['start_date']
-            
-            description = template.format(
-                start_date=start_date,
-                npd=npd
-            ) # full_name=full_name,
-            return description
+        form = self.get_form(self.form_class)
         
-        
+        template = self.get_application().description
 
-        # print(description)
-        # return description
+        # Dynamically create multiple variables
+        for key in form.fields.keys():
+            locals()[key] = form_data[key]
+        print(locals())
+
+        # **locals() syntax, which unpacks the dynamically created variables as keyword arguments.
+        description = template.format(**locals())
+
+
+
+        # description = template.format(
+        #     full_name="Vardenis Pavardenis",
+        #     manager="Direktorius Direktoriauskas",
+        #     start_date=form_data['start_date'],
+        #     end_date=form_data['end_date'],
+        #     date_created=form_data['date_created']
+        # ) # full_name=full_name,
+        return description
     
     
