96dbdd7b725d8aba975a553b5ceb90a72f70e7e0
Kobrius11 <kobrius11@gmail.com>
2023-06-19 15:59:19 +0300

    updated ApplicationFormView

--- JRZ/hr_system/forms.py
+++ JRZ/hr_system/forms.py
@@ -1,9 +1,21 @@
 from django import forms
 from . import models
+from django.utils.translation import gettext_lazy as _
 
 
 class ApplicationInstanceForm(forms.ModelForm):
-    
+    # Vacation
+    start_date = forms.DateField(label=_("Start Date"))
+    end_date = forms.DateField(label=_("End Date"))
+    payout_before = forms.BooleanField(label=_("Payout Before"))
+    #Taxes
+    npd = forms.BooleanField(label=_("npd"))
+    start_date = forms.DateField(label=_("start_date"))
+
+
     class Meta:
         model = models.ApplicationInstance
-        fields = ("application", "date_created", "status", "content")
+        fields = ['content', 'start_date', 'end_date', 'payout_before', 'application']
+        widgets = {
+            'content': forms.HiddenInput(),
+        }
--- JRZ/hr_system/views.py
+++ JRZ/hr_system/views.py
@@ -29,16 +29,74 @@ class ApplicationListView(generic.ListView):
             )
         return qs
 
+# class ApplicationFormView(generic.CreateView):
+#     model = ApplicationInstance
+#     form_class = ApplicationInstanceForm
+#     template_name = 'hr_system/application_instance.html'
+#     success_url = reverse_lazy('application_list')
+
+#     def get_initial(self):
+#         initial = super().get_initial()
+#         obj = get_object_or_404(Application, pk=self.kwargs['pk'])
+#         initial['content'] = obj.description
+#         return initial
+    
 class ApplicationFormView(generic.CreateView):
     model = ApplicationInstance
     form_class = ApplicationInstanceForm
     template_name = 'hr_system/application_instance.html'
     success_url = reverse_lazy('application_list')
 
-    def get_initial(self):
-        initial = super().get_initial()
-        obj = get_object_or_404(Application, pk=self.kwargs['pk'])
-        initial['content'] = obj.description
-        return initial
+    # def get_form(self, form_class= form_class) -> BaseModelForm:
+    #     form = super().get_form(form_class)
+    #     application = get_object_or_404(Application, pk=self.kwargs['pk'])
+    #     if form.application == 1:
+    #         # form = form(initial={'client': forms.HiddenInput() })
+    #         form.fields['client'].widget = forms.HiddenInput() 
+    #     return form
+
+    def form_valid(self, form):
+        # application_id = self.request.POST.get('application')
+        application = get_object_or_404(Application, pk=self.kwargs['pk'])
+        instance = form.save(commit=False)
+        instance.application = application
+        instance.save()
+        # self.content = self.generate_description(form.cleaned_data)
+        form.instance.content = self.generate_description(form.cleaned_data)
+        return super().form_valid(form)
+
+    def generate_description(self, form_data):
+        templates = {'vacation':"Pareigos\nPAKEISK_MANE\n\nUAB ĮMONĖ\ndirektoriui PAKEISK_MANE\n\nPRAŠYMAS DĖL ATOSTOGŲ\n\n{start_date}\n{end_date}\n\nPrašau leisti mane kasmetinių atostogų nuo {start_date} iki {end_date} imtinai. Prašau man priklausančius atostoginius išmokėti kartu su darbo užmokesčio mokėjimu.\n\nPAKEISKMANE\n",
+                     'taxes': 'taxes lalsalsldf'}
+        application = get_object_or_404(Application, pk=self.kwargs['pk'])
+        if application.title == 'Vacation':
+            template = templates['vacation']
+
+            # full_name = f"{self.request.user.first_name} {self.request.user.last_name}"
+            start_date = form_data['start_date']
+            end_date = form_data['end_date']
+
+            description = template.format(
+                start_date=start_date,
+                end_date=end_date
+            ) # full_name=full_name,
+            return description
+
+        if application.title == 'Taxes form':
+            template = templates['taxes']
+
+            npd = form_data['npd']
+            start_date = form_data['start_date']
+            
+            description = template.format(
+                start_date=start_date,
+                npd=npd
+            ) # full_name=full_name,
+            return description
+        
+        
+
+        # print(description)
+        # return description
     
     
